41bdef377b641e7645fc6200200cb1bb89abadd7
JavaDeveloper-Sinku <sinkusingh173@gmail.com>
2026-06-11 11:24:07 +0530

    Add memory-engine Spring Boot service

---
+++ memory-engine/src/main/java/com/memorygraph/memory_engine/MemoryEngineApplication.java
@@ -0,0 +1,14 @@
+package com.memorygraph.memory_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryEngineApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MemoryEngineApplication.class, args);
+	}
+
+}
